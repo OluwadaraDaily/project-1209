a891c1b975b833768892e4af643bc0a44ca44b16
Oluwadara Oloye <daraoloye99@gmail.com>
2024-09-02 00:07:56 +0100

    feature: implement store UI and cart logic

--- src/pages/store.js
+++ src/pages/store.js
@@ -1,11 +1,13 @@
 import { LitElement, css, html } from 'lit'
 import { StoreAPI } from '../services/store';
 import { TWStyles } from '../css/tw';
+import { TProduct } from '../components/product';
 
 export class TStore extends LitElement {
   static get properties() {
     return {
-      products: {}
+      products: {},
+      cart: { state: true },
     }
   }
 
@@ -13,6 +15,7 @@ export class TStore extends LitElement {
     super();
     this.storeAPI = new StoreAPI();
     this.products = [];
+    this.cart = {};
   }
 
   async connectedCallback() {
@@ -20,29 +23,55 @@ export class TStore extends LitElement {
     await this.fetchProducts();
   }
 
+  updateCart(event) {
+    const productId = event.detail.id;
+
+    if (event.type === 'add-to-cart') {
+      this.cart = {
+        ...this.cart,
+        [productId]: (this.cart[productId] + 1) || 1
+      };
+    } else if (event.type === 'remove-from-cart') {
+      if (!this.cart[productId]) return;
+      else {
+        this.cart = {
+            ...this.cart,
+            [productId]: this.cart[productId] - 1
+        };
+      }
+    }
+  }
+
   render() {
     return html`
-      <div class="relative min-h-[100vh] w-full border border-green-900">
-        <h1>This is an empty store...</h1>
+      <div class="relative min-h-[100vh] w-full max-w-[1200px] md:w-[80%] mx-auto my-8">
+        <div>
+          <h1 class="font-semibold mb-8 text-center text-2xl">ALL PRODUCTS</h1>
+          <div class="flex items-stretch justify-center gap-6 flex-wrap">
+            ${this.products.map((product, index) => html`
+              <t-product
+                .product="${product}"
+                .cart="${this.cart}"
+                @add-to-cart="${this.updateCart}"
+                @remove-from-cart="${this.updateCart}"
+              ></t-product>
+            `)}
+          </div>
+        </div>
       </div>
     `
   }
 
   static get styles() {
-    return [css`
-      .text-red-500 {
-        color: red;
-      }
-    `, TWStyles]
+    return [css``, TWStyles]
   }
 
   fetchProducts = async () => {
     try {
       const response = await this.storeAPI.fetchProducts(10);
-      console.log('RESPONSE =>', response);
       this.products = response;
     } catch (error) {
-      console.log('ERROR =>', error);
+      console.error('ERROR =>', error);
     }
   }
 }
